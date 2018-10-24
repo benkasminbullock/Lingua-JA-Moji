93e23a223e70a784a773fdc1dbd799562fa30c5b
Ben Bullock <benkasminbullock@gmail.com>
2018-10-24 11:45:58 +0900

    Documentation, version to 0.55

--- examples/syn-kana.pl
+++ examples/syn-kana.pl
@@ -9,3 +9,5 @@ print kata2hira ($h), "\n";
 print hira2kata (kata2hira ($h)), "\n";
 print kana2hw ($h), "\n";
 print kata2hira (hw2katakana (kana2hw ($h))), "\n";
+# Silly circled kana
+print kana2circled ($h), "\n";
